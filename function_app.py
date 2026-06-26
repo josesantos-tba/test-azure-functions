f95de670a3e6764dc308a383d7eb67392d2a3aae
josesaantos-tba <josesantos@ogrupothebest.com>
2026-06-26 16:17:43 -0300

    .

--- function_app.py
+++ function_app.py
@@ -16,7 +16,7 @@ app.register_functions(protheus_get_table_columns_bp)
 app.register_functions(protheus_generic_query_bp)
 
 @app.function_name(name="openapi_json")
-@app.route(route="openapi.json", auth_level=func.AuthLevel.ANONYMOUS, methods=["GET"])
+@app.route(route="openapi.json", auth_level=func.AuthLevel.FUNCTION, methods=["GET"])
 def openapi_json(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
         get_openapi_json(
@@ -31,7 +31,7 @@ def openapi_json(req: func.HttpRequest) -> func.HttpResponse:
     )
 
 @app.function_name(name="openapi_yaml")
-@app.route(route="openapi.yaml", auth_level=func.AuthLevel.ANONYMOUS, methods=["GET"])
+@app.route(route="openapi.yaml", auth_level=func.AuthLevel.FUNCTION, methods=["GET"])
 def openapi_yaml(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(
         get_openapi_yaml(
@@ -46,28 +46,6 @@ def openapi_yaml(req: func.HttpRequest) -> func.HttpResponse:
     )
 
 @app.function_name(name="swagger_ui")
-@app.route(route="docs", auth_level=func.AuthLevel.ANONYMOUS, methods=["GET"])
+@app.route(route="docs", auth_level=func.AuthLevel.FUNCTION, methods=["GET"])
 def swagger_ui(req: func.HttpRequest) -> func.HttpResponse:
     return render_swagger_ui()
-
-
-@app.route(route="HttpExample", auth_level=func.AuthLevel.FUNCTION)
-def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
